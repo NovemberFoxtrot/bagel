97e651b6621339a9275efd64ce0c3aa1ad7cc5df
NovemberFoxtrot <november@fox.trot>
2014-01-29 18:16:40 -0800

    murk

--- main.go
+++ main.go
@@ -9,11 +9,74 @@ import (
 	"io"
 	"io/ioutil"
 	"log"
+	"net"
+	"net/http"
+	"net/url"
 	"os"
 	"time"
+	"unicode"
 
 	_ "github.com/go-sql-driver/mysql"
 )
+
+type TimeoutConfig struct {
+	ConnectTimeout   time.Duration
+	ReadWriteTimeout time.Duration
+}
+
+func TimeoutDialer(cTimeout time.Duration, rwTimeout time.Duration) func(net, addr string) (c net.Conn, err error) {
+	return func(netw, addr string) (net.Conn, error) {
+		conn, err := net.DialTimeout(netw, addr, cTimeout)
+
+		if err != nil {
+			return nil, err
+		}
+
+		conn.SetDeadline(time.Now().Add(rwTimeout))
+
+		return conn, nil
+	}
+}
+
+func FetchURL(theurl string) string {
+	var client *http.Client
+
+	if proxy := os.Getenv("http_proxy"); proxy != `` {
+		proxyUrl, err := url.Parse(proxy)
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		transport := http.Transport{
+			Dial:  TimeoutDialer(5*time.Second, 5*time.Second),
+			Proxy: http.ProxyURL(proxyUrl),
+		}
+
+		client = &http.Client{Transport: &transport}
+	} else {
+		client = &http.Client{}
+	}
+
+	req, err := http.NewRequest(`GET`, theurl, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return string(body)
+}
 
 type Config struct {
 	Database string `json:"database"`
@@ -94,6 +157,8 @@ func (d *Data) allRows(query string) {
 
 	check(err)
 
+	fmt.Println(len(rows))
+
 	values := make([]sql.RawBytes, len(columns))
 
 	scanArgs := make([]interface{}, len(values))
@@ -125,6 +190,10 @@ func (d *Data) allRows(query string) {
 
 func (d *Data) listCards() {
 	d.allRows(`SELECT * FROM cards;`)
+}
+
+func (d *Data) findCards(text string) {
+	d.allRows(`SELECT * FROM cards WHERE question LIKE BINARY '` + text + `%' limit 10;`)
 }
 
 func (d *Data) listTags() {
@@ -189,7 +258,7 @@ func (d *Data) parseCSV(theFile string) {
 	}
 }
 
-func main() {
+func populate() {
 	var theFile string
 
 	flag.StringVar(&theFile, "f", "file", "")
@@ -215,4 +284,45 @@ func main() {
 	d.listCards()
 	d.listTags()
 	d.listCardTags()
+}
+
+func main() {
+	counter := make(map[string]int, 0)
+
+	data := FetchURL(os.Args[1])
+
+	var buffer []rune
+
+	printed := false
+
+	for _, r := range data {
+		if unicode.Is(unicode.Han, r) || unicode.Is(unicode.Hiragana, r) || unicode.Is(unicode.Katakana, r) || r == 'ー' {
+			buffer = append(buffer, r)
+
+			printed = false
+		} else if printed != true {
+			printed = true
+			counter[string(buffer)] += 1
+			buffer = make([]rune, 0)
+		}
+	}
+
+	var config Config
+
+	config.init()
+
+	var d Data
+
+	d.init(config)
+
+	d.start()
+
+	defer d.stop()
+
+	d.ping()
+
+	for k, v := range counter {
+		fmt.Println(v, k)
+		d.findCards(k)
+	}
 }
